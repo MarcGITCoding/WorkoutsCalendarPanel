1df6e99158d727aed3dfe0f36b01f69aa8cca0c4
Marc <marsuredamunar@gmail.com>
2024-12-05 11:23:37 +0100

    Listener interface for workouts selected

---
+++ src/main/java/msureda/workoutscalendarpanel/WorkoutsCalendarListener.java
@@ -0,0 +1,9 @@
+package msureda.workoutscalendarpanel;
+
+/**
+ * Listener for WorkoutsCalendar
+ * @author Marc Sureda
+ */
+public interface WorkoutsCalendarListener {
+    void workoutsSelected(CalendarEvent event);
+}
